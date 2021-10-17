e59badecfcf5d04ee35293d8455041c54f483796
Ivan Roa <31189913+jeisonroa1@users.noreply.github.com>
2021-10-17 00:41:09 -0500

    Adding V2

--- SparkProject/bootstrap.scala
+++ SparkProject/bootstrap.scala
@@ -1,9 +1,36 @@
-val rddFromFile = spark.sparkContext.textFile("Affairs.csv")
+package com.sparkbyexamples.spark.rdd
 
-val rdd = rddFromFile.map(f=>{
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+object ReadMultipleCSVFiles extends App {
+
+  val spark:SparkSession = SparkSession.builder()
+    .master("local[1]")
+    .appName("SparkByExamples.com")
+    .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  println("spark read csv files from a directory into RDD")
+  val rddFromFile = spark.sparkContext.textFile("Affairs.csv")
+  println(rddFromFile.getClass)
+
+  val rdd = rddFromFile.map(f=>{
     f.split(",")
   })
 
-rdd.foreach(f=>{
+  println("Iterate RDD")
+  rdd.foreach(f=>{
     println("Affairs:"+f(0)+",Gender:"+f(1))
-  })  
+  })
+  println(rdd)
+
+  println("Get data Using collect")
+  rdd.collect().foreach(f=>{
+    println("Affairs:"+f(0)+",Gender:"+f(1))
+  })
+
+  
+
+}
