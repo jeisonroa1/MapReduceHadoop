f3526c24371ddd5ebae7e527cdc753c0862607f1
Ivan Roa <31189913+jeisonroa1@users.noreply.github.com>
2021-10-18 03:08:33 -0500

    Fixing Scala file

--- SparkProject/spark.scala
+++ SparkProject/spark.scala
@@ -26,10 +26,7 @@ val body = headerAndRows.filter(_(0) != header(0));
 val population=body.map(x=>(x(2),x(1).toDouble)).cache();
 //population.foreach(println);
 val pop=population.toDF("gender","affairs");
-val resultPopulation = pop.groupBy("gender").agg(mean("affairs"), variance("affairs"))
-	.sort($"gender").select(
-		col("gender").as("Category"),col("avg(affairs)").as("Mean"),col("var_samp(affairs)")
-		.as("Variance"));
+val resultPopulation = pop.groupBy("gender").agg(mean("affairs"), variance("affairs")).sort($"gender").select(col("gender").as("Category"),col("avg(affairs)").as("Mean"),col("var_samp(affairs)").as("Variance"));
 
 // Equal samples by categorical value
 var males = population.filter(x=>x._1.contains("\"male\""))
@@ -39,18 +36,14 @@ females = females.sample(false,0.25)
 val popSample= males.union(females)
 //popSample.foreach(println);
 var totalElems = 0;
-var finalDf=popSample.sample(true,1).groupByKey().map(x=>(x._1,(getMean(x._2),getVariance(x._2))))
-	.map(a => (a._1, a._2._1, a._2._2)).toDF("Category", "Mean", "Variance");
+var finalDf=popSample.sample(true,1).groupByKey().map(x=>(x._1,(getMean(x._2),getVariance(x._2)))).map(a => (a._1, a._2._1, a._2._2)).toDF("Category", "Mean", "Variance");
 
 
 // Iterations
 for( i <- 1 to 10 ){
 	var rl = List[org.apache.spark.rdd.RDD[(String, (Double, Double))]]();
 
-	(1 to 100).foreach(_ => {var resampleData=popSample.sample(true,1)
-		.groupByKey()
-		.map(x=>(x._1,(getMean(x._2),getVariance(x._2)))); 
-		rl = resampleData :: rl; } );
+	(1 to 100).foreach(_ => {var resampleData=popSample.sample(true,1).groupByKey().map(x=>(x._1,(getMean(x._2),getVariance(x._2)))); rl = resampleData :: rl; } );
 	totalElems = totalElems + rl.length
 
 	val arr = rl.reduce(_ union _).map(a => (a._1, a._2._1, a._2._2));
@@ -67,6 +60,5 @@ resultPopulation.show();
 
 
 println("Result of the sample after bootstraping");
-val resultBoot = finalDf.groupBy("Category").agg(mean("Mean"), 
-	mean("Variance")).sort($"Category");
+val resultBoot = finalDf.groupBy("Category").agg(mean("Mean"), mean("Variance")).sort($"Category");
 resultBoot.show();
